264253a2f966005492e384619d0e2f7867ddd5c5
ProgrammerXD <70265763+naman77s@users.noreply.github.com>
2022-09-02 17:21:15 +0530

    Add files via upload

--- RoadToInfinity/firstGame.js
+++ RoadToInfinity/firstGame.js
@@ -6,12 +6,12 @@ var man = document.getElementById("man");
 var cI = document.getElementById("cI");
 var plr = document.getElementById("plr");
 var bgMusic = document.getElementById("bm");
-/* Starting the game */
+// Starting the game 
 start.addEventListener("click", function(){
 var obs = document.createElement("marquee");
 var garbage = ["🦁","🥫","🔪","🎁","🔥","🪓"];
 
-  /* Creating marquee */
+  // Creating marquee
 for(var i = 0; i < 20; i++){
   var node = document.createElement("font");
   var textnode = document.createTextNode(garbage[Math.floor(Math.random()*garbage.length)]);
@@ -20,7 +20,7 @@ for(var i = 0; i < 20; i++){
    node.appendChild(textnode);
    obs.appendChild(node);
    
-    /* Styling marquee and other elements */
+    // Styling marquee and other elements 
     node.style.color = "blue";
     node.style.fontSize = "25pt";
     node.style.textShadow = "-2px 2px 1px black";
@@ -29,7 +29,7 @@ for(var i = 0; i < 20; i++){
     score.style.display = "flex";
     cI.style.marginTop = "200%";
     cI.style.position = "absolute";
-/* Score js */
+// Score js 
 document.querySelectorAll('.number-animate').forEach( (el) => {
   const endValue = el.getAttribute('data-end-value');
   const incrementValue = el.getAttribute('data-increment');
@@ -52,15 +52,15 @@ function numberAnimation(el, endValue, incrementor, duration) {
     console.log("///Clearing Console");
     console.clear();
 });
-/* Jump function */
-	/* Jump */
+// Jump function 
+	// Jump 
 	window.addEventListener("click", function(){
 	    man.style.marginTop = "35%";
 	    man.style.transition = "ease-out 1s";
-            plr.setAttribute('src','Man2.png');
+            plr.setAttribute('src','Man2.webp');
         console.log("triggered jump");
             console.clear();
-	/* Return to starting point */
+	// Return to starting point 
 	setTimeout(function(){
 	  if(man.style.marginTop <= "60%"){
 	        man.style.marginTop = "65%";
@@ -70,6 +70,6 @@ function numberAnimation(el, endValue, incrementor, duration) {
 	  };},1000);
 	});
 	
-/* Under construction */
-	/* Game over functions */
-        /*"alert("YOUR SCORE : " + count.innerHTML + "\n\n KEEP PLAYING TO BECOME PRO ;p"); */
+/* Under construction 
+	 Game over functions 
+        "alert("YOUR SCORE : " + count.innerHTML + "\n\n KEEP PLAYING TO BECOME PRO ;p"); */
